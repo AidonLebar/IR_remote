62b37b0c5c6e31697a622a288fec87cef3b0ff25
AidonLebar <aidonlebar@gmail.com>
2019-04-04 16:12:51 -0400

    Add skip forward and skip back controls.

--- remote.py
+++ remote.py
@@ -10,17 +10,32 @@ def press(key):
 #press a key with super modifier key
 def super_press(key):
     keyboard.press(Key.cmd_r)
-    keyboard.press(key)
-    keyboard.release(key)
+    press(key)
     keyboard.release(Key.cmd_r)
 
 with serial.Serial(serial_path, 9600) as ser:
     keyboard = Controller()
     while(True):
         button = str(ser.readline(), 'ascii').strip()
-        if(button == "OK"):
+        if(button == "OK"): #play/pause
             press(Key.space)
-        if(button == "U"):
-            super_press(Key.f12)
-        if(button == "D"):
-            super_press(Key.f11)
+        if(button == "U"): #volume up
+            keyboard.press(Key.ctrl_r)
+            keyboard.press(Key.shift_r)
+            keyboard.press(Key.up)
+            keyboard.release(Key.up)
+            keyboard.release(Key.shift_r)
+            keyboard.release(Key.ctrl_r)
+        if(button == "D"): #volume down
+            keyboard.press(Key.ctrl_r)
+            keyboard.press(Key.shift_r)
+            keyboard.press(Key.down)
+            keyboard.release(Key.down)
+            keyboard.release(Key.shift_r)
+            keyboard.release(Key.ctrl_r)
+        if(button == "R"): #skip forward
+            keyboard.press(Key.right)
+            keyboard.release(Key.right)
+        if(button == "L"): #skip back
+            keyboard.press(Key.left)
+            keyboard.release(Key.left)
